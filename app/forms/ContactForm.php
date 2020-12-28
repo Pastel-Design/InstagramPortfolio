efc9f50fba0ccd008ba2fa18e8e305d9729065f5
MakulaVojtech <makulvo@gmail.com>
2020-12-28 13:40:49 +0100

    Added ContactForm.php and basic form has been set up

---
+++ app/forms/ContactForm.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace app\forms;
+
+
+use Nette\Forms\Form;
+
+class ContactForm extends FormFactory
+{
+    private Form $form;
+
+    public function __construct()
+    {
+        $this->form = parent::getForm("contactForm");
+    }
+
+    function create(callable $onSuccess): Form
+    {
+        $this->form->addText("name", "Name")
+            ->setHtmlAttribute("placeholder", "Your name")
+            ->setRequired();
+
+        $this->form->addText("email", "E-mail")
+            ->setHtmlAttribute("placeholder", "Your e-mail")
+            ->setRequired();
+
+        $this->form->addTextArea("message", "Message")
+            ->setHtmlAttribute("placeholder", "Write your message here...")
+            ->setRequired();
+
+        $this->form->addSubmit("submit", "SendMessage");
+
+        //if($this->form->isSubmitted()){ //This if statement is optional
+        if ($this->form->isSuccess()) {
+            // TODO: implement on success behavior
+        }
+        //}
+
+        return $this->form;
+    }
+}
