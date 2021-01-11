b8fd10d71bcaf8505090766acf68914b2a3fbd18
MakulaVojtech <makulvo@gmail.com>
2021-01-11 15:30:13 +0100

    Added photo dialog functionality

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -22,6 +22,7 @@ window.onload = () => {
     let imagesRightButton = document.getElementById("imagesRightButton");
     let imagesLeftButton = document.getElementById("imagesLeftButton");
     let displayedImage = document.querySelector("#photoDialog img");
+    let imgDiv = document.querySelector(".photoInfo");
 
     console.log(photoDialog)
 //Basic setup
@@ -44,6 +45,12 @@ window.onload = () => {
         photo.addEventListener("click", showImageDialog.bind(false, photo.id));
     }
     photoDialog.addEventListener("click", closeImageDialog);
+    imagesLeftButton.addEventListener("click", e => e.stopPropagation());
+    imagesRightButton.addEventListener("click", e => e.stopPropagation());
+    imgDiv.addEventListener("click", e => e.stopPropagation());
+    imagesRightButton.addEventListener("click", changeImageInDialog);
+    imagesLeftButton.addEventListener("click", changeImageInDialog.bind(false, false));
+
 
 //functions
     function setDarkmode() {
@@ -107,6 +114,7 @@ window.onload = () => {
         console.log(photoId)
         if (typeof photoDialog.showModal === "function") {
             displayedImage.src = document.getElementById(photoId).style.backgroundImage.match(/url\(["']?([^"']*)["']?\)/)[1];
+            displayedImage.id = photoId;
             photoDialog.showModal()
             photoDialog.style.display = "flex";
         } else {
@@ -117,5 +125,32 @@ window.onload = () => {
     function closeImageDialog() {
         photoDialog.close();
         photoDialog.style.display = "";
+    }
+
+    function changeImageInDialog(toTheRight = true) {
+        let id;
+        let currentImageId = parseInt(displayedImage.id.match(/\d+$/)[0]);
+        let idNumbers = [];
+        for (let img of photos) {
+            idNumbers.push(parseInt(img.id.match(/\d+$/)[0]));
+        }
+        console.log(idNumbers);
+        if (toTheRight) {
+            if (currentImageId >= Math.max(...idNumbers)) {
+                currentImageId = 0;
+            } else {
+                currentImageId++;
+            }
+        } else {
+            if (currentImageId <= 0) {
+                currentImageId = Math.max(...idNumbers);
+            } else {
+                currentImageId--;
+            }
+        }
+        id = "image_" + currentImageId;
+        displayedImage.id = id;
+        console.log(displayedImage.id);
+        displayedImage.src = document.getElementById(id).style.backgroundImage.match(/url\(["']?([^"']*)["']?\)/)[1];
     }
 }
