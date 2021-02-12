c7683198cad888a9c2e3653d1d524e0155986cad
kukivac <50522557+kukivac@users.noreply.github.com>
2021-02-12 13:29:28 +0100

    deploy fixes

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -171,35 +171,35 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     }
 
+    /* DARKMODE */
+
+    let bodyTag = document.querySelector("body");
+    let darkmodeToggleButton = document.getElementById("darkmodeToggleButton");
+    let isInDark = false;
+
+    let currentTheme = localStorage.getItem("theme") ? localStorage.getItem("theme") : null;
+    if (currentTheme) {
+        if (currentTheme === "dark") {
+            setDarkmode();
+        }
+    }
+    showHighlights();
+
+    window.onload = () => {
+        darkmodeToggleButton.addEventListener("click", setDarkmode);
+    }
+
+    function setDarkmode() {
+        if (!isInDark) {
+            darkmodeToggleButton.classList.replace("fa-toggle-off", "fa-toggle-on");
+            bodyTag.classList.add("darkMode");
+            isInDark = true;
+            localStorage.setItem("theme", "dark");
+        } else {
+            darkmodeToggleButton.classList.replace("fa-toggle-on", "fa-toggle-off");
+            bodyTag.classList.remove("darkMode");
+            isInDark = false;
+            localStorage.setItem("theme", "light");
+        }
+    }
 })
-/* DARKMODE */
-
-let bodyTag = document.querySelector("body");
-let darkmodeToggleButton = document.getElementById("darkmodeToggleButton");
-let isInDark = false;
-
-let currentTheme = localStorage.getItem("theme") ? localStorage.getItem("theme") : null;
-if (currentTheme) {
-    if (currentTheme === "dark") {
-        setDarkmode();
-    }
-}
-showHighlights();
-
-window.onload = () => {
-    darkmodeToggleButton.addEventListener("click", setDarkmode);
-}
-
-function setDarkmode() {
-    if (!isInDark) {
-        darkmodeToggleButton.classList.replace("fa-toggle-off", "fa-toggle-on");
-        bodyTag.classList.add("darkMode");
-        isInDark = true;
-        localStorage.setItem("theme", "dark");
-    } else {
-        darkmodeToggleButton.classList.replace("fa-toggle-on", "fa-toggle-off");
-        bodyTag.classList.remove("darkMode");
-        isInDark = false;
-        localStorage.setItem("theme", "light");
-    }
-}
