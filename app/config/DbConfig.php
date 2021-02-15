36e60da3d4fbd28602369394e2cd5e655eaa2c74
MakulaVojtech <makulvo@gmail.com>
2021-02-15 12:13:11 +0100

    album description

--- app/config/DbConfig.php
+++ app/config/DbConfig.php
@@ -8,14 +8,14 @@ namespace app\config;
  */
 class DbConfig
 {
-    /*
     public static string $host = '127.0.0.1';
     public static string $username = 'root';
     public static string $pass = '';
     public static string $database = 'instagramportfolio';
-    */
+    /*
     public static string $host = '127.0.0.1';
     public static string $username = 'garethjo_root';
     public static string $pass = 'xPassword159753';
     public static string $database = 'garethjo_database';
+    */
 }
